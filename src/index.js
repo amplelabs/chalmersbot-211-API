a61a8cb790d8235356490222b9bae92bbc2ac135
Matthew Wong <matwong@zoho.com>
2018-11-22 19:36:43 -0500

    minor fix

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ const common = require('../lib/common')
 
 console.log('2-1-1 trial api testing')
 
-/*
-axios.get(process.env.TRIAL_API_URL)
-  .then((resp) => {
-    console.log(resp)
-  })
-  .catch(err => console.error(err))
-*/
-
 common.listTopic()
   .then(resp => console.log(resp))
   .catch(err => console.error(err))
