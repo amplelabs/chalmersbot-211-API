9e484aa8e8f23bd8abb18281a03c6f803426ad49
Matthew Wong <matwong@zoho.com>
2018-11-23 20:10:56 -0500

    added meal search and get resourceID functions

--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
 require('dotenv').config()
-const fs = require('fs');
+const fs = require('fs')
 const common = require('../lib/common')
 
 console.log('2-1-1 trial api testing')
-async function main () {
+async function listAllTopics () {
+  console.log('Get all topics and subtopics from 2-1-1 API.')
   try {
     const topics = await common.listTopic().catch(err => console.error(err))
     const fullList = await Promise.all(topics.map(async (x) => {
-      const subTopics = await common.listTopic(x).catch(err => ({[x]:['n/a']}))
+      const subTopics = await common.listTopic(x).catch(() => ({ [x]: ['n/a'] }))
       return {
         [x]: subTopics
       }
       // console.log(subTopics)
     }))
-    const json = JSON.stringify({list: fullList})
-    fs.writeFile("topics.json", json, function(err) {
+    const json = JSON.stringify({ list: fullList })
+    /*
+    fs.writeFile('topics.json', json, function (err) {
       if (err) {
-          console.log(err);
+        console.log(err)
       }
-  });
+    })
+    */
+    console.log(json)
   } catch (e) {
     console.error(e)
   }
 }
 
+async function searchMeals () {
+  const param = {
+    term: '',
+    locationText: 'Toronto',
+    latitude: 45.1510532655634,
+    longitude: -79.398193359375,
+    maxDistanceInKilometers: 10.5,
+    requireWheelchairAccess: false
+  }
+  console.log('moo');
+  const results = await common.searchMeals(param)
+    .catch(err => console.error(err))
+  console.log(results)
+}
+
+async function getResourceInfo() {
+  const result = await common.getResourceInfo('34388-2')
+    .catch(err => console.error(err))
+  console.log(result)
+}
+
+// const main = listAllTopics
+const main = searchMeals
+// const main = getResourceInfo
 main()
