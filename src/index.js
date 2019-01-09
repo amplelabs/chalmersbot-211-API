f2ea2bde399abb5b92d646d057caf65c71922802
dvvrt <dvvrt@users.noreply.github.com>
2019-01-09 00:15:40 -0500

    V1-Mapping-211-Data-To-Chatbot
    Parse `operationHours` for each resource returned by 211 api
    and return a corresponding `meal` object which details required
    by the chabot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const fs = require('fs')
 const common = require('../lib/external')
+const mealTransformer = require('../lib/meal-transformer');
 
 // TODO: put an express server here
 
 console.log('2-1-1 trial api testing')
-async function listAllTopics () {
+async function listAllTopics() {
   console.log('Get all topics and subtopics from 2-1-1 API.')
   try {
     const topics = await common.listTopic().catch(err => console.error(err))
@@ -30,7 +31,7 @@ async function listAllTopics () {
   }
 }
 
-async function searchMeals () {
+async function searchMeals() {
   const param = {
     term: '',
     locationText: 'Toronto',
@@ -39,11 +40,28 @@ async function searchMeals () {
     maxDistanceInKilometers: 10.5,
     requireWheelchairAccess: false
   }
-  const results = await common.searchMeals(param)
-    .catch(err => console.error(err.code)) // log the whole err
+
+  let results;
+
+  try {
+    results = await common.searchMeals(param);
+    const meals = mealTransformer.resourcesToMeal(results);
+    fs.writeFile('meals.json', JSON.stringify(meals, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log(`MealSkills created from 211 api, see meals.json`);
+
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+
   // console.log(results)
   const json = JSON.stringify(results)
-  fs.writeFile('meal211Toronto.json', json, function (err) {
+  // const json = results;
+  fs.writeFile('2_meal211Toronto_1.json', json, function (err) {
     if (err) {
       console.log(err)
     }
@@ -52,12 +70,13 @@ async function searchMeals () {
 }
 
 async function getResourceInfo() {
-  const result = await common.getResourceInfo('34388-2')
+  // const result = await common.getResourceInfo('34388-2')
+  const result = await common.getResourceInfo('6573-0')
     .catch(err => console.error(err.code))
   console.log(result)
 }
 
 // const main = listAllTopics
-// const main = searchMeals
-const main = getResourceInfo
+const main = searchMeals
+// const main = getResourceInfo
 main()
