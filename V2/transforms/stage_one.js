f0bb28db1f2043ce0a9566e43dbd74c97d7e8e1c
dvvrt <dvvrt@users.noreply.github.com>
2019-03-06 23:45:39 -0500

    V2 Data processing, initial work

---
+++ V2/transforms/stage_one.js
@@ -0,0 +1,39 @@
+const _ = require('lodash');
+/**
+ * Converts 211 new (v2) data format into a simpler form where
+ * only attributes of interest are selected
+ * and plain text with markup/special characters is denormalized
+ * into multi-line structure for subsequent processing
+ *
+ * eg.
+ **/
+const stageOneParser = async (raw211Data) => {
+    const selectedData = _.map(raw211Data, (record) => {
+        _.pick(record, [
+            'id_program',
+            'id_cioc',
+            'agency_name',
+            'site_latitude',
+            'site_longitude',
+            'category',
+            'desc_program',
+            'eligibility',
+            'hours',
+            'phone_office',
+            'phone_tollfree',
+            'phone_tty',
+            'fax',
+            'website'
+        ]);
+    });
+
+    const destructuredData = _.map(selectedData, (record) => {
+        record['ampl_hours_details'] = destructure(_.get(record, 'hours'));
+        record['ampl_program_detials'] = destructure(_.get(record, 'desc_program'));
+    })
+
+};
+
+export default [
+    stageOneParser
+];
